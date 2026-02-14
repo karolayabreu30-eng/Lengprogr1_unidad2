f61cfd6cc56576bedec5b4b0243b16c16d0b8ea1
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-13 23:57:21 -0400

    tarea5

---
+++ tarea5/Form1.cs
@@ -0,0 +1,22 @@
+namespace tarea5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                label1.Text = "Seleccionaste Opción 1";
+
+            else if (radioButton2.Checked)
+                label1.Text = "Seleccionaste Opción 2";
+
+            else if (radioButton3.Checked)
+                label1.Text = "Seleccionaste Opción 3";
+        }
+    }
+}
