8ba62946e08cde70e302713156f233ee89d3e2a2
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-13 23:22:38 -0400

    tarea3

---
+++ tarea3/Form1.cs
@@ -0,0 +1,25 @@
+namespace tarea3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+    }
+}
