544fb012aab207802db9b50a0251f5164df2952f
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-15 17:48:23 -0400

    tarea13

---
+++ tarea12/Form1.cs
@@ -0,0 +1,20 @@
+namespace tarea12
+{
+    public partial class Form1 : Form private void button1_Click(object sender, EventArgs e)
+    {
+
+    };
+    F = (C* 9/5) + 32
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
