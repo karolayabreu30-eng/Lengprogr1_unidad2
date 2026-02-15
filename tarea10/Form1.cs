cf84b9197107687e9f1aff78bc53bbd3eff68c45
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-15 16:32:26 -0400

    tarea10

---
+++ tarea10/Form1.cs
@@ -0,0 +1,20 @@
+namespace tarea10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, soy Karolay Abreu y mi matricula es la-2025-00169");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
