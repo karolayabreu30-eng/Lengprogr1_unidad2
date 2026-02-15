b6862bb04cc180dbcbcf1eed741d64ce0602a03d
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-15 18:22:09 -0400

    tarea13

---
+++ tarea13/tarea13/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace tarea13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(97, 24);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(396, 44);
+            progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(113, 156);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 47);
+            button1.TabIndex = 1;
+            button1.Text = "Inciar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(358, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 50);
+            button2.TabIndex = 2;
+            button2.Text = "Detener";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(565, 246);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            Text = "Progressbar";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private ProgressBar progressBar1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ tarea13/tarea13/Form1.cs
@@ -0,0 +1,28 @@
+namespace tarea13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+                progressBar1.Value++;
+            else
+                timer1.Stop();
+        }
+    }
+}
