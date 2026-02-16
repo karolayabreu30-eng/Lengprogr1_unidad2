5a81be3183eb684b2e4170e99ffdea07b173b458
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-15 20:09:26 -0400

    tarea12

--- tarea12/Form1.Designer.cs
+++ tarea12/Form1.Designer.cs
@@ -28,54 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
             button1 = new Button();
             label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(205, 58);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(318, 27);
-            textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(318, 264);
+            button1.Location = new Point(200, 266);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
-            button1.TabIndex = 1;
+            button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += this.button1_Click;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(337, 140);
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(200, 126);
             label1.Name = "label1";
-            label1.Size = new Size(0, 20);
-            label1.TabIndex = 2;
+            label1.Size = new Size(0, 33);
+            label1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(214, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+          
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Highlight;
+            label2.Location = new Point(40, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(156, 31);
+            label2.TabIndex = 3;
+            label2.Text = "Temperatura";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(567, 348);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
-            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
-            Text = "Form1";
+            RightToLeftLayout = true;
+            Text = "Convertir Celsius a Fahrenheit";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
         private Button button1;
         private Label label1;
+        private TextBox textBox1;
+        private Label label2;
     }
 }
